cf68dfb4f798dd70d8b24ca1a57a014a0a74bbe5
Toan Tran <toantdfu@gmail.com>
2018-11-30 13:12:30 +0900

    Correct json property in examples/app.js

--- examples/app.js
+++ examples/app.js
@@ -1,8 +1,8 @@
 var express = require('express'),
-    app = express(),
-    session = require('express-session'),
-    bodyParser = require('body-parser'),
-    creator = require('express-restful-api');
+  app = express(),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  creator = require('express-restful-api');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -10,25 +10,24 @@ app.use(bodyParser.json());
 // register router
 var router = creator.router({
   mongo: process.env.MONGO_URL,
-  schema: {
-
+  schemas: {
     // register company model
     company: {
       name: {
         uniq: true,
         required: true,
         pattern: /^[a-zA-Z _]+$/,
-        desc: "Company name",
-        invalid: "Only alphabets number spaces allowed"
+        desc: 'Company name',
+        invalid: 'Only alphabets number spaces allowed',
       },
       president: {
         type: 'instance',
-        relation: 'persion'
+        relation: 'persion',
       },
       members: {
         type: 'children',
-        relation: 'person'
-      }
+        relation: 'person',
+      },
     },
 
     // register person model
@@ -38,17 +37,17 @@ var router = creator.router({
         uniq: true,
         required: true,
         pattern: /^[a-zA-Z _]+$/,
-        resoure: 'id'
+        resoure: 'id',
       },
       company: {
         type: 'parent',
-        relation: 'company.members'
+        relation: 'company.members',
       },
       age: {
-        type: 'number'
-      }
-    }
-  }
+        type: 'number',
+      },
+    },
+  },
 });
 
 app.use('/', router);
